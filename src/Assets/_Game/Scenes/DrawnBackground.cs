0b2aba591c2d8d9c22438f84582da0697f924465
Jákup Viljam Dam <jayveedees@gmail.com>
2025-09-07 14:06:11 +0100

    adjust main menu background and music

---
+++ src/Assets/_Game/Scenes/DrawnBackground.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Game.Scenes
+{
+    public class DrawnBackground : MonoBehaviour
+    {
+        public float speedX = 50f;
+        public float speedY = 50f;
+        private RectTransform _rect;
+
+        public void Start()
+        {
+            _rect = GetComponent<RectTransform>();
+        }
+
+        public void Update()
+        {
+            _rect.anchoredPosition += new Vector2(speedX, speedY) * Time.deltaTime;
+        }
+    }
+}
