1132072f24078d806db254d14b2b305b70c797d1
Andrias Halsson Joensen <ahj@formula.fo>
2025-09-07 12:39:14 +0100

    Changes for comments

--- src/Assets/_Game/Prefabs/Highscore/HighScoreHandler.cs
+++ src/Assets/_Game/Prefabs/Highscore/HighScoreHandler.cs
@@ -11,15 +11,11 @@ public class HighScoreHandler : MonoBehaviour
     private int _increments = 0;
     private int _score = 0;
 
-    private void Update()
-    {
-        _textMeshPro.SetText($"Score: {GetScore()}%");
-    }
-
     public void AddScore()
     {
         _increments++;
         _score += 1;
+        _textMeshPro.SetText($"Score: {GetScore()}%");
     }
 
     public void RemoveScore()
@@ -31,10 +27,17 @@ public class HighScoreHandler : MonoBehaviour
         {
             _score = 0;
         }
+
+        _textMeshPro.SetText($"Score: {GetScore()}%");
     }
 
     public int GetScore()
     {
+        if (_increments == 0)
+        {
+            return 0;
+        }
+
         return (int)((Mathf.Min(Mathf.Max(0f, (float)_score / (float)_increments), 1f) * 100) + 0.5f);
     }
 }
