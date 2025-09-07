2eaac4958c00cd059ae7e324a092c9a3dc8ff4d4
Jákup Viljam Dam <jayveedees@gmail.com>
2025-09-07 12:35:11 +0100

    Jvd/movement master (#13)
    * make some changes
    
    * randomize from 1 to 2
    
    * Add prefab
    
    * add menu stuff

---
+++ src/Assets/_Game/Scenes/MenuMusic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Game.Scenes
+{
+    public class MenuMusic : MonoBehaviour
+    {
+        [SerializeField]
+        public AudioSource source;
+
+        public void Start()
+        {
+            source.Play();
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
